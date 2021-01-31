63d31042a64841f30e239cc34e67f03c45190407
Ihor kulpekin <konstantin.boykov@pro-store.online>
2021-01-31 15:09:40 +0200

    added infinity scroll with react-lazyload for lazy-loading elements of page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import About from 'components/About/About';
-import ItemPage from "components/ItemPage/ItemPage";
-import films from './films.jpg'
-import ray_img from './ray_people.jpg'
-import starship from './starship.jpg'
-import planet from './planet.jpg'
-import vehicle from './vehicle.jpg'
+import ItemPage from 'components/ItemPage/ItemPage';
+import films from './films.jpg';
+import ray_img from './ray_people.jpg';
+import starship from './starship.jpg';
+import planet from './planet.jpg';
+import vehicle from './vehicle.jpg';
+import LazyLoad from 'react-lazyload';
 
 const Landing = () => (
   <>
-    <About />
-    <ItemPage srcImg={films} title="films"/>
-    <ItemPage srcImg={ray_img} title="characters"/>
-    <ItemPage srcImg={starship} title="starships"/>
-    <ItemPage srcImg={planet} title="planets"/>
-    <ItemPage srcImg={vehicle} title="vehicles"/>
+    <About/>
+    <LazyLoad once={true}>
+      <ItemPage srcImg={films} title="films"/>
+    </LazyLoad>
+    <LazyLoad>
+      <ItemPage srcImg={ray_img} title="characters"/>
+    </LazyLoad>
+    <LazyLoad>
+      <ItemPage srcImg={starship} title="starships"/>
+    </LazyLoad>
+    <LazyLoad>
+      <ItemPage srcImg={planet} title="planets"/>
+    </LazyLoad>
+    <LazyLoad>
+      <ItemPage srcImg={vehicle} title="vehicles"/>
+    </LazyLoad>
   </>
 );
 
