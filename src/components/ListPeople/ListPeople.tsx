d116581d3616c6775364ac52f32529c1f6f68431
Ihor kulpekin <konstantin.boykov@pro-store.online>
2021-01-31 03:20:05 +0200

    added key to item array

--- src/components/ListPeople/ListPeople.tsx
+++ src/components/ListPeople/ListPeople.tsx
@@ -9,7 +9,7 @@ interface ListPeopleProps {
 }
 
 const ListPeople: React.FC<ListPeopleProps> = ({ people, previous, next }) => (
-  <>{people.length !== 0 ? people?.map((person) => <PeopleItem person={person} />) : null}</>
+  <>{people.length !== 0 ? people?.map((person) => <PeopleItem key={person.name} person={person} />) : null}</>
 );
 
 export default ListPeople;
